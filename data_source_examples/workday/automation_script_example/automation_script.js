fd701a3651cb4087be48cd059410ed4d78055878
David Lange <you@example.com>
2022-04-07 14:46:43 -0400

    Adding workday import example

---
+++ data_source_examples/workday/automation_script_example/automation_script.js
@@ -0,0 +1,140 @@
+const {
+    workdayUrl,
+    username,
+    password
+} = input.config();
+
+// this is needed to authenticate the request to the Workday RaaS URL 
+// it's a base64 encoded string of username:password
+const b64encodedString = ''
+
+/*************** HELPER FUNCTIONS ********************************/
+
+// Make request to RaaS URL and return data
+const getWorkdayData = async (url, authKey) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + authKey,
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data['Report_Entry'];
+}
+
+// Takes an array of records and returns a mapping of primary field to record ID
+const createMappingOfUniqueFieldToRecordId = function (records, fieldName) {
+  const mapping = {}
+  for (const existingRecord of records) {
+    mapping[existingRecord.getCellValueAsString(fieldName)] = existingRecord.id
+  }
+  return mapping
+}
+
+// Helper function from https://stackoverflow.com/questions/8495687/split-array-into-chunks
+const chunkArray = function (arrayToChunk, chunkSize = 50) {
+  const arraysOfChunks = []
+  for (var i = 0; i < arrayToChunk.length; i += chunkSize)
+    arraysOfChunks.push(arrayToChunk.slice(i, i + chunkSize))
+  return arraysOfChunks
+}
+
+// Helper functions that takes a table reference and array of un-chunked records
+//   and updates/creates the records
+const updateRecordsInChunks = async function (table, records) {
+  const arrayOfChunks = chunkArray(records)
+  for (const chunkOfRecords of arrayOfChunks) {
+    const updateResult = await table.updateRecordsAsync(chunkOfRecords)
+    // console.info(`🔁  Updated ${chunkOfRecords.length} record(s) in the '${table.name}' table`)
+  }
+}
+const createRecordsInChunks = async function (table, records) {
+  const arrayOfChunks = chunkArray(records)
+  for (const chunkOfRecords of arrayOfChunks) {
+    const createResult = await table.createRecordsAsync(chunkOfRecords)
+    // console.info(`➕  Inserted ${createResult.length} record(s) into the '${table.name}' table`)
+  }
+}
+
+/************* Get Data from Workday *******************************/
+
+// make request to workday and return purchase order data
+const purchaseOrders = await getWorkdayData(workdayUrl, b64encodedString);
+
+// load the Airtable table we need to write data to
+const purchaseOrderTable = base.getTable('Workday Purchase Orders - new');
+
+// create empty array to store transformed data
+const dataForAirtable = [];
+
+// loop through Workday data and transform schema, store in array
+for (const purchase of purchaseOrders) {
+    purchase['Purchase_Order_Line_group'].map(lineItem => {
+        dataForAirtable.push({
+            fields: {
+                'Purchase Order': lineItem.Purchase_Order,
+                'Purchase Order Date': purchase.Purchase_Order_Date,
+                'Buyer': purchase.Buyer,
+                'PO Days Passed': Number(purchase.FIN_DD_PODaysPassed),
+                'Amount Received': Number(lineItem.Amount_Received),
+                'Extended Amount': Number(lineItem.Extended_Amount),
+                'Cost_Center': lineItem.Cost_Center,
+                'Quantity': Number(lineItem.Quantity),
+                'Supplier_Text': lineItem.Supplier ,
+                'Item_Description': lineItem.Item_Description,
+                'Spend_Category_as_Worktag': lineItem.Spend_Category_as_Worktag
+            }
+        })
+    });
+}
+
+/******************************************************************/
+/*
+    The rest of the code in this example is using the best practices
+    we recommend when performing an UPSERT on data (update / create)
+    
+    You can find examples here: https://github.com/Airtable-Labs/upsert-examples
+*/
+
+
+// load existing records in Airtable
+const existingRecords = await purchaseOrderTable.selectRecordsAsync({ fields: ['Unique Key'] });
+
+// create a map object of the primary field and Airtable record id
+/*
+  Example:
+  {
+    PO-10000048: rec12413adwd,
+    PO-100059: rec7637adaad
+  }
+*/
+const mapOfUniqueIdToExistingRecordId = createMappingOfUniqueFieldToRecordId(existingRecords.records, 'Unique Key');
+
+// // create two empty arrays, one for records to create in Airtable and one to update existing records
+const recordsToCreate = [];
+const recordsToUpdate = [];
+
+// // loop through data transformed in previous steps, create unique key to compare against existing data
+for (const record of dataForAirtable) {
+  const uniqueKey = record.fields['Purchase Order'] + '|' + record.fields['Spend_Category_as_Worktag'] + '|' + record.fields['Extended Amount'];
+
+  // find matching key and record ID
+  const recordMatch = mapOfUniqueIdToExistingRecordId[uniqueKey];
+
+  // if no match, create new record, otherwise update existing
+  if (recordMatch === undefined) {
+    recordsToCreate.push(record);
+  } else {
+    recordsToUpdate.push({ id: recordMatch, fields: record.fields });
+  }
+
+}
+
+// output how many records to be created and updated
+console.log(`Records to create: ${recordsToCreate.length}`);
+console.log(`Records to update: ${recordsToUpdate.length}`);
+
+// // create and update records in chunks
+await createRecordsInChunks(purchaseOrderTable, recordsToCreate);
+await updateRecordsInChunks(purchaseOrderTable, recordsToUpdate);
---
+++ data_source_examples/workday/index.js
@@ -0,0 +1,124 @@
+/*
+    This script is an example of how to make a request to a RaaS URL that Workday can provide.
+    With Basic Auth, the data can be retrieved as JSON, and parsed to import into Airtable using
+    the airtable.js library that Airtable supports.
+*/
+
+require('dotenv').config();
+const axios = require('axios');
+const Airtable = require('airtable');
+
+// Load helper functions from helpers.js
+const { createMappingOfUniqueFieldToRecordId, actOnRecordsInChunks } = require('./helpers')
+
+// Read in environment variable values
+const { 
+    AIRTABLE_API_KEY, 
+    AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_ID,
+    AIRTABLE_UNIQUE_FIELD_NAME,
+    WORKDAY_USERNAME,
+    WORKDAY_PASSWORD,
+    WORKDAY_RAAS_URL
+} = process.env;
+
+// Initialize Airtable.js, used for Base APIs
+Airtable.configure({ apiKey: AIRTABLE_API_KEY });
+const base = Airtable.base(AIRTABLE_BASE_ID);
+const table = base(AIRTABLE_TABLE_ID);
+
+// Initialize Axios client, used for Workday RaaS Endpoint
+const axiosClient = axios.create({
+  baseURL: WORKDAY_RAAS_URL,
+  headers: {
+    'Authorization': 'Basic ' + Buffer.from(WORKDAY_USERNAME + ":" + WORKDAY_PASSWORD).toString('base64'),
+    'Content-Type': 'application/json'
+  }
+});
+
+// Helper function to make request to Workday URL
+const getWorkdayData = async () => {
+    try {
+        const response = await axiosClient.get();
+        if (response.status === 200) {
+            console.log('Successful response from Workday');
+            return response.data['Report_Entry'];
+        }
+
+    } catch (err) {
+        throw err;
+    }
+}
+
+(async function () {
+    // Retrieve Workday data
+    const purchaseOrders = await getWorkdayData();
+
+    // Create empty array to story shaped objects of flattened Workday data
+    // this will be easier to work with when performing upsert logic
+    const dataForAirtable = [];
+
+    // Loop through Workday data and flatten the nested object
+    for (const purchaseOrder of purchaseOrders) {
+        purchaseOrder['Purchase_Order_Line_group'].map(lineItem => {
+            dataForAirtable.push({
+                fields: {
+                    'Purchase Order': lineItem.Purchase_Order,
+                    'Purchase Order Date': purchaseOrder.Purchase_Order_Date,
+                    'Buyer': purchaseOrder.Buyer,
+                    'PO Days Passed': Number(purchaseOrder.FIN_DD_PODaysPassed),
+                    'Amount Received': Number(lineItem.Amount_Received),
+                    'Extended Amount': Number(lineItem.Extended_Amount),
+                    'Cost_Center': lineItem.Cost_Center,
+                    'Quantity': Number(lineItem.Quantity),
+                    'Supplier_Text': lineItem.Supplier ,
+                    'Item_Description': lineItem.Item_Description,
+                    'Spend_Category_as_Worktag': lineItem.Spend_Category_as_Worktag
+                }
+            })
+        });
+    }
+
+    /*
+        The rest of the code in this example is using the best practices
+        we recommend when performing an UPSERT on data (update / create)
+        
+        You can find examples here: https://github.com/Airtable-Labs/upsert-examples
+    */
+
+    // Query existing records in Airtable
+    const existingRecords = await table.select().all();
+
+    // Create a map of Airtable Unique Field value and Airtable Record ID
+    const mapOfUniqueIdToExistingRecordId = createMappingOfUniqueFieldToRecordId(existingRecords, AIRTABLE_UNIQUE_FIELD_NAME);
+    
+    // Two empty arrays to store records that will need to be either created or updated
+    const recordsToCreate = [];
+    const recordsToUpdate = [];
+
+    // Loop through Workday data that's been shaped
+    for (const record of dataForAirtable) {
+        // create unique key that exists in Airtable. This will be used to identify whether a record already exists and should be updated
+        const uniqueKey = record.fields['Purchase Order'] + '|' + record.fields['Spend_Category_as_Worktag'] + '|' + record.fields['Extended Amount'];
+          // find matching key and record ID
+        const recordMatch = mapOfUniqueIdToExistingRecordId[uniqueKey];
+
+        // if no match, create new record, otherwise update existing
+        if (recordMatch === undefined) {
+            recordsToCreate.push(record);
+        } else {
+            recordsToUpdate.push({ id: recordMatch, fields: record.fields });
+        }
+     }
+
+    // output how many records to be created and updated
+    console.log(`Records to create: ${recordsToCreate.length}`);
+    console.log(`Records to update: ${recordsToUpdate.length}`);
+
+    // Perform record creation
+    await actOnRecordsInChunks(table, 'create', recordsToCreate);
+
+    // Perform record updates on existing records
+    await actOnRecordsInChunks(table, 'update', recordsToUpdate);
+})();
+
