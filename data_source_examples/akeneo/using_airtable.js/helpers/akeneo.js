e0a0aee64b99b5efc58ba285b1e6c486cd07b393
Mark Silverberg <mark.silverberg@airtable.com>
2023-01-03 08:50:05 -0600

    Akeneo upsert example using Airtable.js (#30)
    * Akeneo upsert example
    * Alphabetize list of data sources
    * Add Akeneo PIM mention in data_source_examples/README.md

---
+++ data_source_examples/akeneo/using_airtable.js/helpers/akeneo.js
@@ -0,0 +1,37 @@
+const getInitialToken = async (akeneoClient, clientId, clientSecret, username, password) => {
+  const initialAuthRequestBasicAuthValue = btoa(`${clientId}:${clientSecret}`)
+  const initialAuthRequestBody = {
+    method: 'POST',
+    url: '/api/oauth/v1/token',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${initialAuthRequestBasicAuthValue}`
+    },
+    data: JSON.stringify({
+      username,
+      password,
+      grant_type: 'password'
+    })
+  }
+  const initiaAuthRequest = await akeneoClient(initialAuthRequestBody)
+  return initiaAuthRequest.data
+}
+
+const getAll = async (akeneoClient, initialPath) => {
+  console.log(`Beginning to paginate off of ${initialPath}`)
+  const allItems = []
+  let nextPageOfItems = initialPath
+  while (nextPageOfItems) {
+    const { data } = await akeneoClient.get(nextPageOfItems)
+    allItems.push(...data._embedded.items)
+    nextPageOfItems = data._links.next?.href
+    console.log(`\t${allItems.length} items gathered`)
+  }
+  console.log(`Gathered total of ${allItems.length}`)
+  return allItems
+}
+
+module.exports = {
+  getAll,
+  getInitialToken
+}
