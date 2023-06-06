2cd220b9a7b08aa1887f2b496e36043cb6ffa81e
AlexCerny-at <98181880+AlexCerny-at@users.noreply.github.com>
2023-06-06 10:00:42 -0600

    added formatting of inputRecords

--- python/using_pyairtable/example.py
+++ python/using_pyairtable/example.py
@@ -19,29 +19,36 @@ Table = Table(AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID)
 inputRecords = [
     # Existing person in the table, if using the sample data linked to in the README
     {
-        "fields": {
-            'First Name': 'Juliette',
-            'Last Name': 'Schimmang',
-            'Unique ID': '16a05ea5-7bbd-4353-bc25-878a2245835e',
-            'Job Title': 'Account Executive II'
-        }
+        'First Name': 'Juliette',
+        'Last Name': 'Schimmang',
+        'Unique ID': '16a05ea5-7bbd-4353-bc25-878a2245835e',
+        'Job Title': 'Account Executive II'
     },
     # New user to be added to the table
     {
-        "fields": {
-            'First Name': 'Marsha',
-            'Last Name': 'Rickeard',
-            'Unique ID': 'bf68da9d-805b-4117-90dc-d54eb46db19f',
-            'Job Title': 'CTO',
-            'Hire Number': 201
-        }
+        'First Name': 'Marsha',
+        'Last Name': 'Rickeard',
+        'Unique ID': 'bf68da9d-805b-4117-90dc-d54eb46db19f',
+        'Job Title': 'CTO',
+        'Hire Number': 201
     }
 ]
 
+# Format inputRecords to include a fields object
+def formatRecords (records):
+    formattedRecords = []
+    for record in records:
+        formattedRecords.append( 
+            dict(fields=record))
+    return formattedRecords
+
+recordsToUpsert = formatRecords(inputRecords)
+print(recordsToUpsert)
+
 # Read out array sizes
-print(f'{len(inputRecords)} records to upsert.')
+print(f'{len(recordsToUpsert)} records to upsert.')
 
 # Perform record upsert
-Table.batch_upsert(inputRecords, ['Unique ID'])
+Table.batch_upsert(recordsToUpsert, ['Unique ID'])
 
 print("\n\nScript execution complete!")
