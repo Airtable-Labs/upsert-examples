6c9d9fd4ffc6d69a432f690639d7fc056e1e6d22
Mark Silverberg <mark.silverberg@airtable.com>
2023-06-21 11:42:18 -0500

    Update node-fetch example to use upsert API (#40)
    * remove unnecessary helper functions; being update to use upsert
    
    * Replace `key` references with `pat` for personal access tokens
    
    Also format README to word wrap
    
    * Rename `AIRTABLE_UNIQUE_FIELD_NAME` to `AIRTABLE_UNIQUE_FIELD_NAME_OR_ID` and associated README updates
    
    * Update README about field types allowed
    
    * Finish implementing upsert logic
    
    Also: simplified helper function call to use more defaults in index.js
    
    * Updated dependency versions
    
    * Ran code linter
    
    * Update README.md

--- javascript/using_node-fetch/helpers.js
+++ javascript/using_node-fetch/helpers.js
@@ -1,40 +1,6 @@
 // Load external dependencies
 const fetch = require('node-fetch')
 
-// Helper function to get all records from a base. Handles pagination for you.
-const getAllRecordsFromBase = async function (baseApiUrl, headers, pageSize = 100, msToSleep = 150) {
-  // Create empty array to hold all records
-  const records = []
-
-  // Set initial request URL
-  let apiRequestUrl = `${baseApiUrl}?pageSize=${pageSize}`
-  console.log(`Retrieving all records for ${apiRequestUrl}`)
-
-  // As long as apiRequestUrl is truthy
-  while (apiRequestUrl) {
-    // Make request to API
-    const apiRequest = await fetch(apiRequestUrl, { headers })
-    const apiResponse = await apiRequest.json()
-
-    // Add records from the API response to array
-    records.push(...apiResponse.records)
-    console.debug(`\trecords array now has ${records.length} items`)
-
-    // Look at response to see if there is another page to fetch
-    if (apiResponse.offset) {
-      apiRequestUrl = `${baseApiUrl}?pageSize=${pageSize}&offset=${apiResponse.offset}`
-      await sleepInMs(msToSleep)
-    } else {
-      console.debug('\tNo further pagination required')
-      apiRequestUrl = null
-    }
-  }
-
-  // Return all records
-  console.debug(`\t${records.length} total records retrieved\n`)
-  return records
-}
-
 // Helper function from https://stackoverflow.com/questions/8495687/split-array-into-chunks
 const chunkArray = function (arrayToChunk, chunkSize = 10) {
   const arraysOfChunks = []
@@ -42,16 +8,15 @@ const chunkArray = function (arrayToChunk, chunkSize = 10) {
   return arraysOfChunks
 }
 
-// Helper function to act on a chnunk of records
-//   method="POST" -> create new records
-//   method="PUT" -> update existing records, will clear all unspecified cell values
-//   method="PATCH" -> update existing records, will only update the fields you specify, leaving the rest as they were
-const actOnRecordsInChunks = async function (baseApiUrl, headers, records, method, msToSleep = 150) {
-  console.log(`\n${method}'ing ${records.length} records at ${baseApiUrl}`)
+// Helper function to upsert a chunk of records
+//   method="PUT" -> upsert (insert-or-update) records, will clear all unspecified cell values
+//   method="PATCH" -> upsert (insert-or-update) records, will only update the fields you specify, leaving the rest as they were
+const upsertRecordsInChunks = async function (baseApiUrl, headers, records, fieldsToMergeOn, msToSleep = 150, method = 'PATCH') {
+  console.log(`\nupsert'ing ${records.length} records at ${method} ${baseApiUrl}`)
   const arrayOfChunks = chunkArray(records)
   for (const chunkOfRecords of arrayOfChunks) {
     console.log(`\tProcessing batch of ${chunkOfRecords.length} records`)
-    const body = JSON.stringify({ records: chunkOfRecords })
+    const body = JSON.stringify({ records: chunkOfRecords, typecast: true, performUpsert: { fieldsToMergeOn } })
     const apiRequest = await fetch(baseApiUrl, { headers, method, body })
     console.log(`\t\t${apiRequest.status} (${apiRequest.statusText})`)
     await sleepInMs(msToSleep)
@@ -63,17 +28,6 @@ async function sleepInMs (ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-// Helper function that takes an array of records and returns a mapping of primary field to record ID
-const createMappingOfUniqueFieldToRecordId = function (records, fieldName) {
-  const mapping = {}
-  for (const existingRecord of records) {
-    mapping[existingRecord.fields[fieldName]] = existingRecord.id
-  }
-  return mapping
-}
-
 module.exports = {
-  getAllRecordsFromBase,
-  createMappingOfUniqueFieldToRecordId,
-  actOnRecordsInChunks
+  upsertRecordsInChunks
 }
--- javascript/using_node-fetch/index.js
+++ javascript/using_node-fetch/index.js
@@ -5,19 +5,17 @@ require('dotenv').config()
 const fetch = require('node-fetch')
 
 // Load helper functions from helpers.js
-const { getAllRecordsFromBase, createMappingOfUniqueFieldToRecordId, actOnRecordsInChunks } = require('./helpers')
+const { upsertRecordsInChunks } = require('./helpers')
 
 // Define variables for base API and default headers
-const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID, AIRTABLE_UNIQUE_FIELD_NAME, AIRTABLE_API_MS_TO_SLEEP } = process.env
+const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID, AIRTABLE_UNIQUE_FIELD_NAME_OR_ID, AIRTABLE_API_MS_TO_SLEEP } = process.env
 const baseApiUrl = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_ID}`
 const airtableAuthHeaders = new fetch.Headers({
   Authorization: `Bearer ${AIRTABLE_API_KEY}`,
   'Content-Type': 'application/json'
 })
 
-  ;
-
-(async () => {
+; (async () => {
   // Define input records (from the source system). This would usually be an API call or reading in a CSV or other format of data.
   const inputRecords = [
     // Existing person in the table, if using the sample data linked to in the README
@@ -37,45 +35,14 @@ const airtableAuthHeaders = new fetch.Headers({
     }
   ]
 
-  // Retrieve all existing records from the base through the Airtable REST API
-  const existingRecords = await getAllRecordsFromBase(baseApiUrl, airtableAuthHeaders, 100, AIRTABLE_API_MS_TO_SLEEP)
+  // Format record objects to match Airtable API format
+  const recordsToUpsert = inputRecords.map(r => ({ fields: r }))
 
-  // Create an object mapping of the primary field to the record ID
-  // Remember, it's assumed that the AIRTABLE_UNIQUE_FIELD_NAME field is truly unique
-  const mapOfUniqueIdToExistingRecordId = createMappingOfUniqueFieldToRecordId(existingRecords, AIRTABLE_UNIQUE_FIELD_NAME)
+  // Read out array size
+  console.log(`\nRecords to upsert: ${recordsToUpsert.length}`)
 
-  // Create two arrays: one for records to be created, one for records to be updated
-  const recordsToCreate = []
-  const recordsToUpdate = []
-
-  // For each input record, check if it exists in the existing records. If it does, update it. If it does not, create it.
-  console.log(`Processing ${inputRecords.length} input records to determine whether to update or create`)
-  for (const inputRecord of inputRecords) {
-    const recordUniqueFieldValue = inputRecord[AIRTABLE_UNIQUE_FIELD_NAME]
-    console.debug(`\tProcessing record w/ '${AIRTABLE_UNIQUE_FIELD_NAME}' === '${recordUniqueFieldValue}'`)
-    // Check for an existing record with the same unique ID as the input record
-    const recordMatch = mapOfUniqueIdToExistingRecordId[recordUniqueFieldValue]
-
-    if (recordMatch === undefined) {
-      // Add record to list of records to update
-      console.log('\t\tNo existing records match; adding to recordsToCreate')
-      recordsToCreate.push({ fields: inputRecord })
-    } else {
-      // Add record to list of records to create
-      console.log(`\t\tExisting record w/ ID ${recordMatch} found; adding to recordsToUpdate`)
-      recordsToUpdate.push({ id: recordMatch, fields: inputRecord })
-    }
-  }
-
-  // Read out array sizes
-  console.log(`\nRecords to create: ${recordsToCreate.length}`)
-  console.log(`Records to update: ${recordsToUpdate.length}\n`)
-
-  // Perform record creation
-  await actOnRecordsInChunks(baseApiUrl, airtableAuthHeaders, recordsToCreate, 'POST', AIRTABLE_API_MS_TO_SLEEP)
-
-  // Perform record updates on existing records
-  await actOnRecordsInChunks(baseApiUrl, airtableAuthHeaders, recordsToUpdate, 'PATCH', AIRTABLE_API_MS_TO_SLEEP)
+  // Perform record upserts
+  await upsertRecordsInChunks(baseApiUrl, airtableAuthHeaders, recordsToUpsert, [AIRTABLE_UNIQUE_FIELD_NAME_OR_ID], AIRTABLE_API_MS_TO_SLEEP)
 
   console.log('\n\nScript execution complete!')
 })()
