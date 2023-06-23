20bbc10987ef345c00f1eb2adc22c8617dce0d31
David Lange <dlange8181@gmail.com>
2023-06-23 17:24:49 -0400

    Davidl update airtable js (#41)
    * Updating example to use performUpsert
    
    * updating .env example
    
    * Updating README and env.example file
    
    minor fixes to reference the new upsert functionality and personal access token

--- javascript/using_airtable.js/index.js
+++ javascript/using_airtable.js/index.js
@@ -5,10 +5,10 @@ require('dotenv').config()
 const Airtable = require('airtable')
 
 // Load helper functions from helpers.js
-const { createMappingOfUniqueFieldToRecordId, actOnRecordsInChunks } = require('./helpers')
+const { upsertRecordsInChunks } = require('./helpers')
 
 // Define variables and initialize Airtable client
-const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID, AIRTABLE_UNIQUE_FIELD_NAME } = process.env
+const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_ID, AIRTABLE_UNIQUE_FIELD_NAME_OR_ID } = process.env
 Airtable.configure({ apiKey: AIRTABLE_API_KEY })
 const base = Airtable.base(AIRTABLE_BASE_ID)
 const table = base(AIRTABLE_TABLE_ID)
@@ -35,45 +35,14 @@ const table = base(AIRTABLE_TABLE_ID)
     }
   ]
 
-  // Retrieve all existing records from the base through the Airtable REST API
-  const existingRecords = await table.select().all()
+  // Format record objects to match Airtable API format
+  const recordsToUpsert = inputRecords.map(r => ({ fields: r }))
 
-  // Create an object mapping of the primary field to the record ID
-  // Remember, it's assumed that the AIRTABLE_UNIQUE_FIELD_NAME field is truly unique
-  const mapOfUniqueIdToExistingRecordId = createMappingOfUniqueFieldToRecordId(existingRecords, AIRTABLE_UNIQUE_FIELD_NAME)
+  // Read out array size
+  console.log(`\nRecords to upsert: ${recordsToUpsert.length}`)
 
-  // Create two arrays: one for records to be created, one for records to be updated
-  const recordsToCreate = []
-  const recordsToUpdate = []
-
-  // For each input record, check if it exists in the existing records. If it does, update it. If it does not, create it.
-  console.log(`Processing ${inputRecords.length} input records to determine whether to update or create`)
-  for (const inputRecord of inputRecords) {
-    const recordUniqueFieldValue = inputRecord[AIRTABLE_UNIQUE_FIELD_NAME]
-    console.debug(`\tProcessing record w/ '${AIRTABLE_UNIQUE_FIELD_NAME}' === '${recordUniqueFieldValue}'`)
-    // Check for an existing record with the same unique ID as the input record
-    const recordMatch = mapOfUniqueIdToExistingRecordId[recordUniqueFieldValue]
-
-    if (recordMatch === undefined) {
-      // Add record to list of records to update
-      console.log('\t\tNo existing records match; adding to recordsToCreate')
-      recordsToCreate.push({ fields: inputRecord })
-    } else {
-      // Add record to list of records to create
-      console.log(`\t\tExisting record w/ ID ${recordMatch} found; adding to recordsToUpdate`)
-      recordsToUpdate.push({ id: recordMatch, fields: inputRecord })
-    }
-  }
-
-  // Read out array sizes
-  console.log(`\nRecords to create: ${recordsToCreate.length}`)
-  console.log(`Records to update: ${recordsToUpdate.length}\n`)
-
-  // Perform record creation
-  await actOnRecordsInChunks(table, 'create', recordsToCreate)
-
-  // Perform record updates on existing records
-  await actOnRecordsInChunks(table, 'update', recordsToUpdate)
+  // Perform record upserts
+  await upsertRecordsInChunks(table, recordsToUpsert, [AIRTABLE_UNIQUE_FIELD_NAME_OR_ID])
 
   console.log('\n\nScript execution complete!')
 })()
