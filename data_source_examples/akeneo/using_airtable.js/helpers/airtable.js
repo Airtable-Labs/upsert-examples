a58a287e6f0fe17b865164da803dd1c9a1129ca1
Mark Silverberg <mark.silverberg@airtable.com>
2023-06-27 09:21:45 -0500

    Update upsert helper to be consistent
    with generic example: https://github.com/Airtable-Labs/upsert-examples/blob/main/javascript/using_airtable.js/helpers.js

--- data_source_examples/akeneo/using_airtable.js/helpers/airtable.js
+++ data_source_examples/akeneo/using_airtable.js/helpers/airtable.js
@@ -1,16 +1,23 @@
-// credit https://stackoverflow.com/questions/8495687/split-array-into-chunks
+// Helper function from https://stackoverflow.com/questions/8495687/split-array-into-chunks
 const chunkArray = function (arrayToChunk, chunkSize = 10) {
   const arraysOfChunks = []
   for (let i = 0; i < arrayToChunk.length; i += chunkSize) { arraysOfChunks.push(arrayToChunk.slice(i, i + chunkSize)) }
   return arraysOfChunks
 }
 
+// Helper function to upsert a chunk of records
+//   method="PUT" -> upsert (insert-or-update) records, will clear all unspecified cell values
+//   method="PATCH" -> upsert (insert-or-update) records, will only update the fields you specify, leaving the rest as they were
 const upsertRecordsInChunks = async function (table, records, fieldsToMergeOn) {
   console.log(`\nupsert'ing ${records.length} records`)
   const arrayOfChunks = chunkArray(records)
   for (const chunkOfRecords of arrayOfChunks) {
     console.log(`\tProcessing batch of ${chunkOfRecords.length} records`)
-    await table.update(chunkOfRecords, { typecast: true, performUpsert: { fieldsToMergeOn } })
+    try {
+      await table.update(chunkOfRecords, {'performUpsert': {'fieldsToMergeOn': fieldsToMergeOn}});
+    } catch (error) {
+      console.error(`Error occurred while upserting records: ${error.message}`);
+    }
   }
 }
 
