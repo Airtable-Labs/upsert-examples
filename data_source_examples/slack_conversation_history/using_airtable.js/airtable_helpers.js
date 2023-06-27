161bd3eb07f0f25b9e47009e147835b333924836
Mark Silverberg <mark.silverberg@airtable.com>
2023-06-27 11:35:41 -0500

    Simplify code to use upsert endpoint; format/lint too

--- data_source_examples/slack_conversation_history/using_airtable.js/airtable_helpers.js
+++ data_source_examples/slack_conversation_history/using_airtable.js/airtable_helpers.js
@@ -5,32 +5,22 @@ const chunkArray = function (arrayToChunk, chunkSize = 10) {
   return arraysOfChunks
 }
 
-// Helper function to act on a chnunk of records
-const actOnRecordsInChunks = async function (table, createOrUpdate, records) {
-  console.log(`\n${createOrUpdate}'ing ${records.length} records`)
+// Helper function to upsert a chunk of records
+//   method="PUT" -> upsert (insert-or-update) records, will clear all unspecified cell values
+//   method="PATCH" -> upsert (insert-or-update) records, will only update the fields you specify, leaving the rest as they were
+const upsertRecordsInChunks = async function (table, records, fieldsToMergeOn) {
+  console.log(`\nupsert'ing ${records.length} records`)
   const arrayOfChunks = chunkArray(records)
   for (const chunkOfRecords of arrayOfChunks) {
     console.log(`\tProcessing batch of ${chunkOfRecords.length} records`)
-    if (createOrUpdate === 'create') {
-      await table.create(chunkOfRecords, { typecast: true }) // typecast=true so that we can specify values instead of record IDs with the Airtable REST API
-    } else if (createOrUpdate === 'update') {
-      await table.update(chunkOfRecords, { typecast: true })
-    } else {
-      throw new Error(`Unexpected value for createOrUpdate: ${createOrUpdate}`)
+    try {
+      await table.update(chunkOfRecords, { performUpsert: { fieldsToMergeOn }, typecast: true })
+    } catch (error) {
+      console.error(`Error occurred while upserting records: ${error.message}`)
     }
   }
 }
 
-// Helper function that takes an array of records and returns a mapping of primary field to record ID
-const createMappingOfUniqueFieldToRecordId = function (records, fieldName) {
-  const mapping = {}
-  for (const existingRecord of records) {
-    mapping[existingRecord.fields[fieldName]] = existingRecord.id
-  }
-  return mapping
-}
-
 module.exports = {
-  createMappingOfUniqueFieldToRecordId,
-  actOnRecordsInChunks
+  upsertRecordsInChunks
 }
