cd4b568b10e2203abe70bc12f9a2d3372f4eeade
Mark Silverberg <mark.silverberg@airtable.com>
2023-06-27 10:23:15 -0500

    Update code to use upsert endpoint

--- data_source_examples/figma/using_airtable.js/airtable_helpers.js
+++ data_source_examples/figma/using_airtable.js/airtable_helpers.js
@@ -1,28 +1,22 @@
 // Helper function from https://stackoverflow.com/questions/8495687/split-array-into-chunks
 const chunkArray = function (arrayToChunk, chunkSize = 10) {
   const arraysOfChunks = []
-  for (let i = 0; i < arrayToChunk.length; i += chunkSize) {
-    arraysOfChunks.push(arrayToChunk.slice(i, i + chunkSize))
-  }
+  for (let i = 0; i < arrayToChunk.length; i += chunkSize) { arraysOfChunks.push(arrayToChunk.slice(i, i + chunkSize)) }
   return arraysOfChunks
 }
 
-// Helper function to act on a chnunk of records
-const actOnRecordsInChunks = async function (table, createOrUpdate, records) {
-  // console.log(`\t${createOrUpdate}'ing ${records.length} records`)
+// Helper function to upsert a chunk of records
+//   method="PUT" -> upsert (insert-or-update) records, will clear all unspecified cell values
+//   method="PATCH" -> upsert (insert-or-update) records, will only update the fields you specify, leaving the rest as they were
+const upsertRecordsInChunks = async function (table, records, fieldsToMergeOn) {
+  console.log(`\nupsert'ing ${records.length} records`)
   const arrayOfChunks = chunkArray(records)
   for (const chunkOfRecords of arrayOfChunks) {
-    // console.log(`\t\tProcessing batch of ${chunkOfRecords.length} records`)
+    console.log(`\tProcessing batch of ${chunkOfRecords.length} records`)
     try {
-      if (createOrUpdate === 'create') {
-        await table.create(chunkOfRecords, { typecast: true }) // typecast=true so that we can specify values instead of record IDs with the Airtable REST API
-      } else if (createOrUpdate === 'update') {
-        await table.update(chunkOfRecords, { typecast: true })
-      } else {
-        throw new Error(`Unexpected value for createOrUpdate: ${createOrUpdate}`)
-      }
-    } catch (err) {
-      console.error('\t\t\t!!! Error occured while creating or updating records')
+      await table.update(chunkOfRecords, { performUpsert: { fieldsToMergeOn }, typecast: true })
+    } catch (error) {
+      console.error(`Error occurred while upserting records: ${error.message}`)
     }
   }
 }
@@ -36,52 +30,7 @@ const createMappingOfUniqueFieldToRecordId = function (records, fieldName) {
   return mapping
 }
 
-// Helper function to update or create records
-async function upsertRecords (table, tableNameOrId, airtableUniqueFieldName, inputRecords) {
-  console.log(`Upserting records on table '${tableNameOrId}'`)
-
-  // Retrieve all existing records from the base through the Airtable REST API
-  const existingRecords = await table.select().all()
-
-  // Create an object mapping of the primary field to the record ID
-  // Remember, it's assumed that the AIRTABLE_UNIQUE_FIELD_NAME field is truly unique
-  const mapOfUniqueIdToExistingRecordId = createMappingOfUniqueFieldToRecordId(existingRecords, airtableUniqueFieldName)
-
-  // Create two arrays: one for records to be created, one for records to be updated
-  const recordsToCreate = []
-  const recordsToUpdate = []
-
-  // For each input record, check if it exists in the existing records. If it does, update it. If it does not, create it.
-  console.log(`\tProcessing ${inputRecords.length} input records to determine whether to update or create in table '${tableNameOrId}'`)
-  for (const inputRecord of inputRecords) {
-    const recordUniqueFieldValue = inputRecord[airtableUniqueFieldName]
-    // console.debug(`\t\tProcessing record w/ '${airtableUniqueFieldName}' === '${recordUniqueFieldValue}'`)
-    // Check for an existing record with the same unique ID as the input record
-    const recordMatch = mapOfUniqueIdToExistingRecordId[recordUniqueFieldValue]
-
-    if (recordMatch === undefined) {
-      // Add record to list of records to update
-      // console.log('\t\t\tNo existing records match; adding to recordsToCreate')
-      recordsToCreate.push({ fields: inputRecord })
-    } else {
-      // Add record to list of records to create
-      // console.log(`\t\t\tExisting record w/ ID ${recordMatch} found; adding to recordsToUpdate`)
-      recordsToUpdate.push({ id: recordMatch, fields: inputRecord })
-    }
-  }
-
-  // Read out array sizes
-  console.log(`\n\tRecords to create in table '${tableNameOrId}': ${recordsToCreate.length}`)
-  console.log(`\tRecords to update in table '${tableNameOrId}': ${recordsToUpdate.length}\n`)
-
-  // Perform record creation
-  await actOnRecordsInChunks(table, 'create', recordsToCreate)
-
-  // Perform record updates on existing records
-  await actOnRecordsInChunks(table, 'update', recordsToUpdate)
-}
-
 module.exports = {
-  createMappingOfUniqueFieldToRecordId,
-  upsertRecords
+  upsertRecordsInChunks,
+  createMappingOfUniqueFieldToRecordId
 }
--- data_source_examples/figma/using_airtable.js/index.js
+++ data_source_examples/figma/using_airtable.js/index.js
@@ -6,7 +6,7 @@ const Airtable = require('airtable')
 const { Client } = require('figma-js')
 
 // Load helper functions from *_helpers.js
-const { createMappingOfUniqueFieldToRecordId, upsertRecords } = require('./airtable_helpers')
+const { upsertRecordsInChunks, createMappingOfUniqueFieldToRecordId } = require('./airtable_helpers')
 
 // Define variables and initialize Airtable client
 const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env
@@ -20,6 +20,7 @@ const figma = Client({ personalAccessToken: FIGMA_PERSONAL_ACCESS_TOKEN })
 // Configuration object containing table and field names/IDs.
 // By having this object defined, there is one place to update if you modify the template base's schema/naming.
 // (You do not need to edit this if you did not change table names or fields from the duplicated example base.)
+// Airtable recommends using table and field IDs instead of names. The example below uses names for readability.
 const TABLES_AND_FIELDS = {
   PROJECTS: {
     TABLE_NAME: 'Figma Projects',
@@ -58,18 +59,21 @@ const TABLES_AND_FIELDS = {
   const { data: { projects: figmaProjectsForTeam } } = await figma.teamProjects(FIGMA_TEAM_ID)
   const inputProjectRecords = figmaProjectsForTeam.map((p) => {
     return {
-      [TABLES_AND_FIELDS.PROJECTS.FIELDS.DISPLAY_NAME]: p.name,
-      [TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_ID]: p.id,
-      [TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_TEAM_ID]: FIGMA_TEAM_ID
+      fields: {
+        [TABLES_AND_FIELDS.PROJECTS.FIELDS.DISPLAY_NAME]: p.name,
+        [TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_ID]: p.id,
+        [TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_TEAM_ID]: FIGMA_TEAM_ID
+      }
     }
   })
 
   // Use upsertRecords helper function to update existing or create new project records
   const airtableProjectsTableNameOrId = TABLES_AND_FIELDS.PROJECTS.TABLE_NAME
   const airtableProjectsTable = base.table(airtableProjectsTableNameOrId)
-  await upsertRecords(airtableProjectsTable, airtableProjectsTableNameOrId, TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_ID, inputProjectRecords)
+  await upsertRecordsInChunks(airtableProjectsTable, inputProjectRecords, [TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_ID])
 
   // === FIGMA FILES ===
+
   // Retrieve latest list of records from the projects table in Airtable
   const airtableProjectRecords = await airtableProjectsTable.select().all()
   const airtableProjectRecordsMappingOfUniqueIdToRecordId = createMappingOfUniqueFieldToRecordId(airtableProjectRecords, TABLES_AND_FIELDS.PROJECTS.FIELDS.FIGMA_ID)
@@ -82,11 +86,13 @@ const TABLES_AND_FIELDS = {
     const { data: { files: figmaFilesForProject } } = await figma.projectFiles(figmaProjectId)
     const inputFileRecordsForProject = figmaFilesForProject.map((f) => {
       return {
-        [TABLES_AND_FIELDS.FILES.FIELDS.DISPLAY_NAME]: f.name,
-        [TABLES_AND_FIELDS.FILES.FIELDS.FIGMA_ID]: f.key,
-        [TABLES_AND_FIELDS.FILES.FIELDS.LINKED_RECORD_TO_PROJECT]: [airtableRecordId],
-        // Sometimes a thumbnail_url is not available from Figma, so populare the Airtable field only when a thumbnail is non-null
-        ...(f.thumbnail_url && { [TABLES_AND_FIELDS.FILES.FIELDS.THUMBNAIL]: [{ url: f.thumbnail_url }] })
+        fields: {
+          [TABLES_AND_FIELDS.FILES.FIELDS.DISPLAY_NAME]: f.name,
+          [TABLES_AND_FIELDS.FILES.FIELDS.FIGMA_ID]: f.key,
+          [TABLES_AND_FIELDS.FILES.FIELDS.LINKED_RECORD_TO_PROJECT]: [airtableRecordId],
+          // Sometimes a thumbnail_url is not available from Figma, so populate the Airtable field only when a thumbnail is non-null
+          ...(f.thumbnail_url && { [TABLES_AND_FIELDS.FILES.FIELDS.THUMBNAIL]: [{ url: f.thumbnail_url }] })
+        }
       }
     })
     inputFileRecords.push(...inputFileRecordsForProject)
@@ -95,10 +101,11 @@ const TABLES_AND_FIELDS = {
   // Use upsertRecords helper function to update existing or create new file records
   const airtableFilesTableNameOrId = TABLES_AND_FIELDS.FILES.TABLE_NAME
   const airtableFilesTable = base.table(airtableFilesTableNameOrId)
-  await upsertRecords(airtableFilesTable, airtableFilesTableNameOrId, TABLES_AND_FIELDS.FILES.FIELDS.FIGMA_ID, inputFileRecords)
+  await upsertRecordsInChunks(airtableFilesTable, inputFileRecords, [TABLES_AND_FIELDS.FILES.FIELDS.FIGMA_ID])
 
   if (POPULATE_PAGES_TABLE === 'true') {
     // === FIGMA PAGES ===
+
     // Retrieve latest list of records from the files table in Airtable
     const airtableFileRecords = await airtableFilesTable.select().all()
     const airtableFileRecordsMappingOfUniqueIdToRecordId = createMappingOfUniqueFieldToRecordId(airtableFileRecords, TABLES_AND_FIELDS.FILES.FIELDS.FIGMA_ID)
@@ -115,9 +122,11 @@ const TABLES_AND_FIELDS = {
       // Format array of pages into Airtable record field-value pairs
       const inputPageRecordsForFile = figmaPagesForFile.map((p) => {
         return {
-          [TABLES_AND_FIELDS.PAGES.FIELDS.DISPLAY_NAME]: p.name,
-          [TABLES_AND_FIELDS.PAGES.FIELDS.FIGMA_FILE_ID_PLUS_NODE_RANGE]: `${figmaFileId} ${p.id}`, // because node range by itself is not unique
-          [TABLES_AND_FIELDS.PAGES.FIELDS.LINKED_RECORD_TO_FILE]: [airtableRecordId]
+          fields: {
+            [TABLES_AND_FIELDS.PAGES.FIELDS.DISPLAY_NAME]: p.name,
+            [TABLES_AND_FIELDS.PAGES.FIELDS.FIGMA_FILE_ID_PLUS_NODE_RANGE]: `${figmaFileId} ${p.id}`, // because node range by itself is not unique
+            [TABLES_AND_FIELDS.PAGES.FIELDS.LINKED_RECORD_TO_FILE]: [airtableRecordId]
+          }
         }
       })
       inputPageRecords.push(...inputPageRecordsForFile)
@@ -126,7 +135,7 @@ const TABLES_AND_FIELDS = {
     // Use upsertRecords helper function to update existing or create new file records
     const airtablePagesTableNameOrId = TABLES_AND_FIELDS.PAGES.TABLE_NAME
     const airtablePagesTable = base.table(airtablePagesTableNameOrId)
-    await upsertRecords(airtablePagesTable, airtablePagesTableNameOrId, TABLES_AND_FIELDS.PAGES.FIELDS.FIGMA_FILE_ID_PLUS_NODE_RANGE, inputPageRecords)
+    await upsertRecordsInChunks(airtablePagesTable, inputPageRecords, [TABLES_AND_FIELDS.PAGES.FIELDS.FIGMA_FILE_ID_PLUS_NODE_RANGE])
   }
 
   console.log('\nScript execution complete')
