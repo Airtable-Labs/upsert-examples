4e63b2e679834d284e2de538f2282eeed38c5add
AlexCerny-at <98181880+AlexCerny-at@users.noreply.github.com>
2023-07-10 14:51:50 -0600

    Updating aha example to use upsert API (#51)

--- data_source_examples/aha/using_pyairtable/example.py
+++ data_source_examples/aha/using_pyairtable/example.py
@@ -64,65 +64,16 @@ for record in inputRecords:
     record['Progress Source'] = feature.get('progress_source')
 print('Feature details retrieved')
 
-# Retrieve all existing records from the base through the Airtable REST API
-allExistingRecords = Table.all()
-print(f'All existing records retrieved. Count: {len(allExistingRecords)}')
-
-# Create an object mapping of the primary field to the record ID
-# Remember, it's assumed that the AIRTABLE_UNIQUE_FIELD_NAME field is truly unique
-upsertFieldValueToExistingRecordId = {
-    existingRecord['fields'].get(AIRTABLE_UNIQUE_FIELD_NAME): existingRecord['id'] for existingRecord in allExistingRecords
-}
-
-# Create two arrays: one for records to be created, one for records to be updated
-recordsToCreate = []
-recordsToUpdate = []
-
-# For each input record, check if it exists in the existing records. If it does, update it. If it does not, create it.
-for inputRecord in inputRecords:
-    recordUniqueValue = inputRecord.get(AIRTABLE_UNIQUE_FIELD_NAME, None)
-    # print('\tProcessing record w / \'{}\' === \'{}\''.format(
-    #     AIRTABLE_UNIQUE_FIELD_NAME, recordUniqueValue))
-
-    existingRecordIdBasedOnUpsertFieldValueMaybe = upsertFieldValueToExistingRecordId.get(
-        recordUniqueValue)
-
-    # and if the upsert field value matches an existing one...
-    if existingRecordIdBasedOnUpsertFieldValueMaybe:
-        # Add record to list of records to update
-        recordsToUpdate.append(
-            dict(id=existingRecordIdBasedOnUpsertFieldValueMaybe, fields=inputRecord))
-    else:
-        # Otherwise, add record to list of records to create
-        recordsToCreate.append(inputRecord)
+# Format inputRecords to include a fields object
+recordsToUpsert = []
+for record in inputRecords:
+    recordsToUpsert.append( 
+        dict(fields=record))
 
 # Read out array sizes
-print('\n{} records to create'.format(len(recordsToCreate)))
-print('{} records to update'.format(len(recordsToUpdate)))
+print(f'{len(recordsToUpsert)} records to upsert.')
 
-# Perform record creation
-Table.batch_create(recordsToCreate)
+# Perform record upsert
+Table.batch_upsert(recordsToUpsert, [AIRTABLE_UNIQUE_FIELD_NAME])
 
-# Perform record updates on existing records
-Table.batch_update(recordsToUpdate)
-
-# Uncomment the following block of code if you wish to set records in Airtable # that do not exist in
-# your inputRecords dataset. The example code assumes you have a checkbox field named 'Inactive'
-# # Create sets of unique ID values for both input and existing records
-# inputRecordsUpsertFieldValues = set([
-#     d[AIRTABLE_UNIQUE_FIELD_NAME] for d in inputRecords])
-# allExistingRecordsUpsertFieldValues = set([
-#     d['fields'][AIRTABLE_UNIQUE_FIELD_NAME] for d in allExistingRecords])
-# # Determine which unique ID values are not present in input records
-# existingRecordsUpsertFieldValuesNotPresentInInputRecords = allExistingRecordsUpsertFieldValues.difference(
-#     inputRecordsUpsertFieldValues)
-# # Create list of dictionaries representing Airtable update record API payloads
-# recordsToUpdateAsInactive = [{'id': upsertFieldValueToExistingRecordId.get(
-#     recordUniqueValue), 'fields': {'Inactive': True}}
-#     for recordUniqueValue in existingRecordsUpsertFieldValuesNotPresentInInputRecords]
-# print('\n{} records to set Inactive=true (unique ID exists in Airtable records but not in input data source)'.format(
-#     len(recordsToUpdateAsInactive)))
-# # Perform record updates on existing records that are now considered 'Inactive'
-# Table.batch_update(recordsToUpdateAsInactive)
-
-print('\n\nScript execution complete!')
+print("\n\nScript execution complete!")
